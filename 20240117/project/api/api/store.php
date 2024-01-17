0153cce1336828d94c1bbd20b2bb7ce414180f8f
YIHONG <akashidaiki2@gmail.com>
2024-01-17 14:36:54 +0800

    post 的 crud

---
+++ 20240117/project/api/api/store.php
@@ -0,0 +1,16 @@
+<?php
+// $data = $_POST;
+
+$data = [
+  'acc' => 'admin',
+  'pwd' => '12345'
+];
+$sql = "SELECT * FROM `user` WHERE acc = 'admin'";
+
+
+//
+
+// php
+$url = header('Location: .....');
+
+// sql = "insert into table column value cloumn"....
---
+++ 20240117/project/api/login/login.php
@@ -0,0 +1,62 @@
+<?php
+// login
+// $data = $_POST;
+
+
+$data = [
+    'acc' => 'kai',
+    'pw' => '12345'
+];
+
+print_r($data);
+
+// 驗證資料
+
+// 抓資料來驗證
+
+// sql .....
+
+// 帳號 密碼 是否正確
+// ok  ....
+// error ...
+
+// 驗證資料
+// 抓資料來驗證
+// sql .....
+
+// 帳號 密碼 是否正確
+// ok  ....
+// error ...
+
+
+// 洗衣服
+
+// 1.拿出全部的衣服
+// 2.分類
+switch ($variable) {
+    case '上衣':
+        # code...
+        break;
+    case '褲子':
+        # code...
+        break;
+
+    default:
+        # code...
+        break;
+}
+// 3.摺衣服
+    // 上衣 ....
+    // 褲子 ....
+
+
+// 4.放到櫃子
+    // 上衣 ....
+    // 褲子 ....
+
+
+// 洗衣服
+// 1.function 拿出全部的衣服
+// 2.function 分類
+// 3.function 摺衣服
+// 4.function 褲子
